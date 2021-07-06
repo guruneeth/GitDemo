88543011d9234b42761ae4afc14b1e109fc0cbe0
guruneeth <guruneet.savant@gmail.com>
2021-07-07 00:14:09 +0530

    This is first commit

---
+++ src/test/java/mavenSample/Mavenjava/AppiumTest.java
@@ -0,0 +1,20 @@
+package mavenSample.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+
+	public void NativeAppAndroid() {
+
+		System.out.println("NativeAppAndroid");
+
+	}
+
+	@Test
+	public void IOSAppp() {
+		System.out.println("IOSAppp");
+
+	}
+}
